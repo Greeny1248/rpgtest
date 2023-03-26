c0c6691d0ab8c99d444ee833277c55e1ba2a161f
Josh <greeny1248@gmail.com>
2023-03-26 18:29:02 +0100

    Adding plan

---
+++ spiking.js
@@ -0,0 +1,49 @@
+/*
+
+
+JavaScript
+phaser/pixie/canvas
+backend?
+
+
+
+Top down or 2.5D Isometric?
+Medieval Fantasy
+WASD movement 
+Mouse to attack
+
+
+
+Combat Phase 1
+Click mouse to swing/shoot/magic
+three classes
+Warrior (sword)
+Ranger (bow)
+Mage (magic)
+(Medic in towns to heal? or Pots)
+
+Combat Phase 2
+Look into block/dodge
+special moves
+potions
+
+
+Plan, 
+
+Spiking, what tech is needed for project
+
+1a: Map moves around player with WASD
+b: add sword swing attacks infront of player depending on direction
+c: add health eg:100hp
+d: add damage: 20hp
+
+2a: Add an enemy
+b: If enemy touches player, player loses 20hp
+c: add enemy health eg:40hp
+
+
+
+
+
+
+*/
